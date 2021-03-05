591a53e82e19ae701e8e5694270500ec1629e1b0
Nwadike Philip <nwadike@epicdevler.com>
2021-03-05 15:19:02 +0100

    First Commit

---
+++ app/src/main/java/com/epicdevler/calc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.epicdevler.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
